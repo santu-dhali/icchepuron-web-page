9fede804411ca38c0ed6003f957fdf7e0e89dddc
santu <santudhali958@gmail.com>
2024-06-12 21:20:27 +0530

    Education Section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Body from "./components/Body";
 import Member from "./components/Member";
+import Education from "./components/Education";
 
 
 const AppLayout = ()=>{
   return(
     <div className="App">
       <Header/>
-      <Body/>
+      <Education/>
       <Footer/>
     </div>
   )
